98eb75b2ffd76496f108e4bcacb93c1d0366346b
kaskar2008 <kaskar2008@gmail.com>
2016-04-05 05:12:37 +0300

    All 'if' are changed to 'if...else if'

--- server/common.js
+++ server/common.js
@@ -10,44 +10,37 @@ Router.route('/tblogin/', {where: "server"})
 	.post(function () {
 		var reservedFields = ['authToken', 'botCode'];
 		var params = this.request.body;
-
-		if (params === undefined || params === null) {
-			this.response.statusCode = 400;
-			this.response.end('No params provided');
-			return;
-		}
-
-		var authToken = params.authToken;
-		var botCode = params.botCode;
-
-		if (authToken === undefined || authToken === null ||
-				botCode === undefined || botCode === null) {
-			this.response.statusCode = 400;
-			this.response.end('No auth token or bot code provided');
-			return;
-		}
-
-		var user = {
-			username: params.username,
-			password: botCode,
-			email: "",
-			profile: {
-				name: params.username,
-				tgId: params.tgId,
-				role: 'User'
-			}
-		};
-
-		if (authToken !== tgbot_authToken) {
-			this.response.statusCode = 403;
-			this.response.end('Bad token');
-			return;
-		}
-
 		var res = this.response;
 
-		Meteor.call("_TGlogin", botCode, user, function() {
-			res.statusCode = 200;
-			res.end("[Telegram login] botCode: " + botCode + "\n");
-		});
+		if (params === undefined || params === null) {
+			res.statusCode = 400;
+			res.end('No params provided');
+			return;
+		} else
+		if (params.authToken === undefined || params.authToken === null ||
+			params.botCode === undefined || params.botCode === null) {
+			res.statusCode = 400;
+			res.end('No auth token or bot code provided');
+			return;
+		} else
+		if (params.authToken !== tgbot_authToken) {
+			res.statusCode = 403;
+			res.end('Bad token');
+			return;
+		} else {
+			Meteor.call("_TGlogin", botCode, user, function() {
+				var user = {
+					username: params.username,
+					password: botCode,
+					email: "",
+					profile: {
+						name: params.username,
+						tgId: params.tgId,
+						role: 'User'
+					}
+				};
+				res.statusCode = 200;
+				res.end("[Telegram login] botCode: " + botCode + "\n");
+			});
+		}
 	});
